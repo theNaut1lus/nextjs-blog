8f54ffe674d3561deeb03ed241260ca6d1d8b1f7
Sidney Aulakh <sidneyaulakh@gmail.com>
2023-03-28 16:48:26 +1100

    dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,20 @@
+import utilStyles from "../../styles/utils.module.css";
+
+import Head from "next/head";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+
+import Date from "../../components/date";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
 
 export default function Post({ postData }) {
   return (
@@ -7,14 +22,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
@@ -24,15 +38,5 @@ export async function getStaticPaths() {
   return {
     paths,
     fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-
-  return {
-    props: {
-      postData,
-    },
   };
 }
